e53ca3e93a76477a76360a3b34f1f6962a107799
Eelke van der Horst <eelke@thehyve.nl>
2025-12-12 14:42:52 +0100

    Reverted "aandoening" prefix

--- scripts/fdp_test_data_upload.py
+++ scripts/fdp_test_data_upload.py
@@ -181,7 +181,7 @@ token = get_token(f"{baseurl}tokens")
 # Publicatiedatum;Aandoening;ICD-10;Indicatie;Behandeling;RxNorm;
 for row in pakketadviezen:
     
-    keywords = [ "Aandoening: " + row["Aandoening"].strip(), 
+    keywords = [ row["Aandoening"].strip(), 
                 # row["Indicatie"].strip(), 
                 row["Behandeling"].strip(), 
                 row["Geneesmiddel"].strip() ]
